81f2beaedf6b26a432b4a9b2aa24a400b892cd37
Neha Muley <nehamuley955@gmail.com>
2024-10-04 13:32:31 +0530

    rate`Limit

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
+const { rateLimit } = require("express-rate-limit")
 const publicController = require("../controller/public.controller")
 
 router
     .get("/fetch-project", publicController.fetchProjects)
     .get("/fetch-caro", publicController.getAllCarousel)
     .get("/project-details/:id", publicController.getProjectDetail)
-    .post("/add-enquery", publicController.fetchProjects)
+    .post("/add-enquery", rateLimit({ windowMs: 15 * 60 * 1000, limit: 1 }), publicController.AddEnqueryMessage)
 
 module.exports = router
 
